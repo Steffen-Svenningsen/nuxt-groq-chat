d58444d503221b7a04fab28a39eb1b318365da49
Steffen Svenningsen <stef7625@gmail.com>
2024-03-25 20:13:38 +0100

    Minor UI update testing

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,21 @@ const { messages, input, handleSubmit } = useChat();
 </script>
 
 <template>
-  <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-    <div v-for="m in messages" key="m.id" class="whitespace-pre-wrap">
-      {{ m.role === 'user' ? 'User: ' : 'AI: ' }}
-      {{ m.content }}
+  <div class="flex flex-col w-full max-w-md py-12 px-4 mx-auto stretch box-border">
+    <div v-for="m in messages" key="m.id" class="whitespace-pre-wrap my-8">
+      <div class="m-header flex gap-2 items-center mb-2">
+        <img class="w-8 h-8" v-if="m.role === 'assistant'" src="/pages/assets/images/chatbot.png" alt="AI">
+        <img class="w-8 h-8" v-else src="/pages/assets/images/avatar.png" alt="User">
+        {{ m.role === 'user' ? 'User: ' : 'AI: ' }}
+      </div>
+      <div>
+        {{ m.content }}
+      </div>
     </div>
 
     <form @submit="handleSubmit">
       <input
-        class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+        class="fixed border-box bottom-0 w-full mx-auto max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
         v-model="input"
         placeholder="Say something..."
       />
